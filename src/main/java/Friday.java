8e263032ae3e56ce9e2a6908ae8d23b8af2db49e
hyder <amirhyderfeisal@gmail.com>
2026-01-22 17:59:34 +0800

    Level 1

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        Scanner sc = new Scanner(System.in);
 
         String line = "--------------------------------------------";
-        System.out.println(line);
-        System.out.println("Hello! I'm Friday");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        String indentation = "  ";
+        System.out.println(indentation + line);
+        System.out.println(indentation + "Hello! I'm Friday");
+        System.out.println(indentation + "What can I do for you?");
+        System.out.println(indentation + line);
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(indentation + line);
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(indentation + input);
+                System.out.println(indentation + line);
+            }
+        }
+        System.out.println(indentation + "Bye. Hope to see you again soon!");
+        System.out.println(indentation + line);
     }
 }
