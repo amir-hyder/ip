bba1d18de79e4001f2184129b3aace1acd36cfe4
hyder <amirhyderfeisal@gmail.com>
2026-01-22 17:25:33 +0800

    Level 0

---
+++ src/main/java/Friday.java
@@ -0,0 +1,18 @@
+public class Friday {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+
+        String line = "--------------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Friday");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
