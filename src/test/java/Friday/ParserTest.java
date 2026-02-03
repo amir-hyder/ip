c02cf89f069cc6c5f57ceab8a886d5b54238d998
hyder <amirhyderfeisal@gmail.com>
2026-02-04 00:57:09 +0800

    Add JUnit tests

---
+++ src/test/java/Friday/ParserTest.java
@@ -0,0 +1,26 @@
+package Friday;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    public void parseIndex_valid_returnsNumber() throws FridayException {
+        Parser parser = new Parser();
+        assertEquals(2, parser.parseIndex("mark 2"));
+    }
+
+    @Test
+    public void parseIndex_missingNumber_throws() {
+        Parser parser = new Parser();
+        assertThrows(FridayException.class, () -> parser.parseIndex("mark"));
+    }
+
+    @Test
+    public void parseIndex_nonInteger_throws() {
+        Parser parser = new Parser();
+        assertThrows(FridayException.class, () -> parser.parseIndex("mark two"));
+    }
+}
---
+++ src/test/java/Friday/TaskListTest.java
@@ -0,0 +1,31 @@
+package Friday;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskListTest {
+
+    @Test
+    public void addTask_increasesSize() {
+        TaskList list = new TaskList();
+        assertEquals(0, list.size());
+
+        list.addTask(new ToDo("read book"));
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void deleteTask_removesCorrectTask() {
+        TaskList list = new TaskList();
+        list.addTask(new ToDo("a"));
+        list.addTask(new ToDo("b"));
+
+        // your deleteTask expects 1-based index
+        list.deleteTask(1);
+
+        assertEquals(1, list.size());
+        assertTrue(list.get(0).toString().contains("b"));
+    }
+}
+
